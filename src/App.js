7bcf3350b51193685c4e3a71dd03c552566d641c
Tech Youths <arsalanmohd237@gmail.com>
2021-06-15 15:22:04 +0530

    Added Navigation and images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navigation from './components/Navigation';
 
 const App = () => {
 	return (
-		<>
+		<>	
 		  <Router>
+		       <Navigation />
              <Switch> 
                 <Route path="/" component={Home} exact></Route>
                 <Route path="/about" component={About}></Route>
